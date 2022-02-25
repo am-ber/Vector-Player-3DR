bead7e97a9e1b936800b6b6c0cd2a49d1b33737e
Zach Vanscoit <zachary816@gmail.com>
2022-02-25 15:17:04 -0500

    created initial file structure

---
+++ VP3DR-Solution/Vector-Library/DefaultScene.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector_Library
+{
+	internal class DefaultScene : IScene
+	{
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Draw()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ VP3DR-Solution/Vector-Library/IScene.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector_Library
+{
+	public interface IScene
+	{
+		public void Draw();
+		public void Update();
+		public void Dispose();
+	}
+}
