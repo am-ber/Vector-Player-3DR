814deac2b1ed454a79e48a351bf81b82f17c2a36
Zach Vanscoit <zachary816@gmail.com>
2022-03-02 15:36:08 -0500

    added terrain example

---
+++ VP3DR-Solution/TerrainScene/TerrainScene.cs
@@ -0,0 +1,24 @@
+﻿using Vector_Library;
+
+namespace Terrain
+{
+	public class TerrainScene : IScene
+	{
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Draw()
+		{
+			// code to make pretty terrain
+			throw new NotImplementedException();
+		}
+
+		public void Update()
+		{
+
+			throw new NotImplementedException();
+		}
+	}
+}
